beb6b0cfdae5906f8eed5e47c71be920f806fb67
umesh.mishra <umesh.mishra@globant.com>
2025-05-22 19:44:24 +0530

    Changes commited for test cases

--- src/test/java/com/codedecode/restaurantlisting/RestaurantlistingApplicationTests.java
+++ src/test/java/com/codedecode/restaurantlisting/RestaurantlistingApplicationTests.java
@@ -1,13 +1,31 @@
 package com.codedecode.restaurantlisting;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+
+@ExtendWith(MockitoExtension.class)
 class RestaurantlistingApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @Test
+    void mainTest() {
+        try (MockedStatic<SpringApplication> mockedStatic =
+                     Mockito.mockStatic(SpringApplication.class)) {
+
+            mockedStatic
+                    .when(() -> SpringApplication.run(RestaurantlistingApplication.class, eq(Mockito.any())))
+                    .thenReturn(null);
+
+            RestaurantlistingApplication.main(new String[] {});
+            RestaurantlistingApplication application = new RestaurantlistingApplication();
+            assertNotNull(application);
+        }
+    }
 
 }
